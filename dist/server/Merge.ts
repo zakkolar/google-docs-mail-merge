8d448ea749b16c6df6e0d51143d19a8f3fd9755f
Zak Kolar <zak.kolar@gmail.com>
2019-06-06 21:07:43 -0400

    simplified traversal

--- dist/server/Merge.ts
+++ dist/server/Merge.ts
@@ -33,61 +33,44 @@ function addMergeData(doc){
 
     let data = mergeData.next();
 
-    // Depth First Search
-    const elements = [body];
-        while(elements.length>0){
-            const el = elements.shift();
-            if(el.getNumChildren){
-                const children = el.getNumChildren();
-                for(let i=children-1; i>=0; i--){
-                    elements.unshift(el.getChild(i));
+    const searchPattern = "\{\{.[^\}]*\}\}";
+    while(body.findText(searchPattern)){
+        const range = body.findText(searchPattern);
+
+        const start = range.getStartOffset();
+        const end = range.getEndOffsetInclusive();
+        const placeholder = range.getElement().getText().substring(start, end + 1);
+
+            // @ts-ignore
+            if(placeholder == makeNextPlaceholder()) {
+                if (mergeData.hasNext()) {
+                    data = mergeData.next();
+                } else {
+                    data = emptyField;
                 }
+                replaceTextInEl(range.getElement(), start, end, "");
             }
-            else{
-                if(el.findText){
-                    const searchPattern = "\{\{.[^\}]*\}\}";
-                    while(el.findText(searchPattern)){
-                        const range = el.findText(searchPattern);
-
-                        const start = range.getStartOffset();
-                        const end = range.getEndOffsetInclusive();
-                        const placeholder = range.getElement().getText().substring(start, end + 1);
-
-                            // @ts-ignore
-                            if(placeholder == makeNextPlaceholder()) {
-                                if (mergeData.hasNext()) {
-                                    data = mergeData.next();
-                                } else {
-                                    data = emptyField;
-                                }
-                                replaceTextInEl(range.getElement(), start, end, "");
-                            }
-                            // @ts-ignore
-                           else if(placeholder == makeNextHiddenPlaceholder()) {
-                                if (mergeData.hasNext()) {
-                                    data = mergeData.next();
-                                } else {
-                                    data = emptyField;
-                                }
-                                //remove or text stays hidden in document??
-                                replaceTextInEl(range.getElement(), start, end, "");
-                                range.getElement().getParent().removeChild(range.getElement());
-                            }
-                            // @ts-ignore
-                           else if(fields.indexOf(removePlaceholderTags(placeholder))>-1){
-                                // @ts-ignore
-                                replaceTextInEl(range.getElement(), start, end, data[removePlaceholderTags(placeholder)]);
-                            }
-                           else{
-                                // @ts-ignore
-                                replaceTextInEl(range.getElement(), start, end, "UNKNOWN PLACEHOLDER: "+removePlaceholderTags(placeholder));
-                            }
-
-                        }
-
-
+            // @ts-ignore
+           else if(placeholder == makeNextHiddenPlaceholder()) {
+                if (mergeData.hasNext()) {
+                    data = mergeData.next();
+                } else {
+                    data = emptyField;
                 }
+                //remove or text stays hidden in document??
+                replaceTextInEl(range.getElement(), start, end, "");
+                range.getElement().getParent().removeChild(range.getElement());
             }
+            // @ts-ignore
+           else if(fields.indexOf(removePlaceholderTags(placeholder))>-1){
+                // @ts-ignore
+                replaceTextInEl(range.getElement(), start, end, data[removePlaceholderTags(placeholder)]);
+            }
+           else{
+                // @ts-ignore
+                replaceTextInEl(range.getElement(), start, end, "UNKNOWN PLACEHOLDER: "+removePlaceholderTags(placeholder));
+            }
+
         }
 
 
